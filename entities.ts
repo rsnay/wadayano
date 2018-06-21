4c8cb0f7577763b8fb59fc4c290e37bfbe310b68
Robert Nay <rsn.nay@gmail.com>
2018-06-21 13:15:06 -0600

    Entity tweaks

--- entities.ts
+++ entities.ts
@@ -1,43 +1,35 @@
 export interface Option {
-  id: number,
+  id: string,
   isCorrect: boolean,
-  questionId: number,           //NOTE: this is the simplest way, but won't allow easy recycling of distractors.
+  questionId: string,           //NOTE: this is the simplest way, but won't allow easy recycling of distractors.
   text: string,                         //e.g. "Riboflavin"
 }
 
 export interface Question {
-  id: number,
+  id: string,
   options: Option[],
   prompt: string,           //e.g. "Which of the following is not included in the B Vitamin complex?"
-  conceptId: number           //NOTE: this approach suggests 1-to-1 question-concept
+  conceptId: string           //NOTE: this approach suggests 1-to-1 question-concept
 }
 
 export interface Concept {
-  id: number,
+  id: string,
   title: string,            //e.g. "Micronutrients"
 }
 
 export interface Quiz {
-  id: number,
+  id: string,
   available: number,         //UTC when this quiz becomes available in the course (NOTE: this is a naive approach)
   questions: Question[],
   isGraded: boolean,        //i.e. does this quiz return a grade via LTI? Or just a practice quiz?
   title: string,            //e.g. "Micronutrients"
 }
 
-export interface QuestionAttempt {
-  userId: number,
-  questionId: number,
-  selectedOption: number,   //option.id
-  isCorrect?: boolean       //optional; for convenience
-  isConfident: boolean      //i.e. "I'm confident my answer is correct"
-  timestamp: number         //UTC
-}
-
 export interface QuizAttempt {
-  userId: number,
-  quizId: number,
-  quizAttempts: QuizAttempt[],
+  id: string,
+  userId: string,
+  quizId: string,
+  questionAttempts: QuestionAttempt[],
   score: number,                 // (correct/total)
   conceptConfidences: ConceptConfidence[],
   totalConfidenceError: number,  // sum of forEach(attempt =>  abs(isConfident - isCorrect)). 0 == perfect estimation accuracy
@@ -45,19 +37,34 @@ export interface QuizAttempt {
   timestamp: number              //UTC
 }
 
+export interface QuestionAttempt {
+  id: string,
+  questionId: string,
+  selectedOption: number,   //option.id
+  isCorrect?: boolean       //optional; for convenience
+  isConfident: boolean      //i.e. "I'm confident my answer is correct"
+  timestamp: number         //UTC
+}
+
 export interface ConceptConfidence {
-  conceptId: number,
+  id: string,
+  conceptId: string,
   conceptTitle?: string,        //optional; for convenience
   confidenceError: number,    //same as above, but filtered to a single concept
   confidenceBias: number,     //ditto
 }
 
 export interface Course {     //LTI login grants access to one course
-  id: number,
-  quizzes: Quiz[]
+  id: string,
+  title: string,
+  instructorId: string,
+  quizzes: Quiz[],
+  concepts: Concept[]
 }
 
 export interface User {
-   id: number,
+   id: string,
+   email: string,
+   password: string,
    role: string                 //"instructor" | "student"
 }
