89a311ed13dec573c73cb796f1e6a6512ad73d09
Robert Nay <rsn.nay@gmail.com>
2018-06-27 01:56:28 -0600

    Add mock quiz data for testing

--- client/src/components/student/QuizTaker.js
+++ client/src/components/student/QuizTaker.js
@@ -3,18 +3,74 @@ import { Link } from 'react-router-dom';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
+import QuestionView from './QuestionView';
+
+import mockData from '../../mockData';
+
+// Different phases or stages of the quiz-taking experience
+let phases = {
+  CONCEPTS: 'concepts',
+  QUESTIONS: 'questions',
+  RESULTS: 'results',
+  CONCEPT_REVIEW: 'concept_review'
+};
+
 export default class QuizTaker extends Component {
-  state = {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      phase: phases.QUESTIONS,
+      conceptConfidences: [],
+      currentQuestionIndex: 0,
+      questionAttempts: [],
+      quizAttempt: null
+    };
+  }
+
+  // Called from a QuestionView after its question has been answered, confidence-rated, and reviewed
+  _onNextQuestion() {
+    // If at the end of the quiz...
+    let newIndex = this.state.currentQuestionIndex + 1;
+    // ... go to results (still set new currentQuestionIndex so progress bar fills up)
+    if (newIndex >= this.props.quiz.questions.length) {
+      this.setState({ phase: phases.RESULTS, currentQuestionIndex: newIndex });
+    } else {
+      // Otherwise go to next question
+      this.setState({ currentQuestionIndex: newIndex });
+    }
   }
 
   render() {
+
+    let currentView;
+    switch (this.state.phase) {
+      case phases.CONCEPTS:
+        currentView = 'Concepts'
+        break;
+      case phases.QUESTIONS:
+        currentView = <QuestionView
+          question={this.props.quiz.questions[this.state.currentQuestionIndex]}
+          key={this.state.currentQuestionIndex}
+          _onNextQuestion={() => this._onNextQuestion() } />;
+        break;
+      case phases.RESULTS:
+        currentView = 'Results view';
+        break;
+      case phases.CONCEPT_REVIEW:
+        currentView = 'Concept review';
+        break;
+    }
+
     return (
         <section class="section">
         <div class="container">
-          <h1 class="title is-inline-block">Not a real quiz</h1>
+          <h1 class="title">{this.props.quiz.title}</h1>
+          <i>{this.props.quiz.questions.length} questions</i>
+          <progress className="progress is-link" value={this.state.currentQuestionIndex} max={this.props.quiz.questions.length}></progress>
           <hr />
 
-          <p>This will contain the quiz-taking experience</p>
+          {currentView}
 
           <hr />
 
@@ -29,3 +85,7 @@ export default class QuizTaker extends Component {
     )
   }
 }
+
+QuizTaker.defaultProps = {
+  quiz: mockData.quizzes[0]
+};
---
+++ client/src/mockData.js
@@ -0,0 +1,80 @@
+export default {
+    "concepts": [
+        {
+            "id": "c1",
+            "title": "Capital Cities"
+        }
+    ],
+    "quizzes": [
+    {
+      "id": "testid",
+      "available": "2018-06-25 03:02:01",
+      "isGraded": true,
+      "title": "State Capitals",
+      "questions": [
+        {
+          "id": "q1",
+          "conceptId": "c1",
+          "prompt": "What is the capital city of Utah?",
+          "options": [
+            {
+              "id": "o1",
+              "isCorrect": false,
+              "questionId": "q1",
+              "text": "Boise"
+            },
+            {
+              "id": "o2",
+              "isCorrect": false,
+              "questionId": "q1",
+              "text": "Denver"
+            },
+            {
+              "id": "o3",
+              "isCorrect": true,
+              "questionId": "q1",
+              "text": "Salt Lake City"
+            },
+            {
+              "id": "o4",
+              "isCorrect": false,
+              "questionId": "q1",
+              "text": "Provo"
+            }
+          ]
+        },
+        {
+          "id": "q2",
+          "conceptId": "c1",
+          "prompt": "What is the capital city of Idaho?",
+          "options": [
+            {
+              "id": "o5",
+              "isCorrect": true,
+              "questionId": "q1",
+              "text": "Boise"
+            },
+            {
+              "id": "o6",
+              "isCorrect": false,
+              "questionId": "q1",
+              "text": "Little Rock"
+            },
+            {
+              "id": "o7",
+              "isCorrect": false,
+              "questionId": "q1",
+              "text": "Ogden"
+            },
+            {
+              "id": "o8",
+              "isCorrect": false,
+              "questionId": "q1",
+              "text": "Spudville"
+            }
+          ]
+        }
+      ]
+    }
+    ]
+  }
