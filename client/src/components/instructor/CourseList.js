f2254a3938e011fdae76b82428b1619294799230
Robert Nay <rsn.nay@gmail.com>
2018-07-03 13:45:54 -0600

    Add course creation card, and add course cards from mock data

--- client/src/components/instructor/CourseList.js
+++ client/src/components/instructor/CourseList.js
@@ -4,51 +4,62 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import AuthCheck from './AuthCheck';
+import CreateCourseForm from './CreateCourseForm';
 
 export default class CourseList extends Component {
-  state = {
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
 
   render() {
+
+    // TODO actually get data from API
+    const courses = [
+        {id: 'course1', title: 'Example Course 1', quizzes: [1,2,3]},
+        {id: 'course2', title: 'Example Course 2', quizzes: [1]},
+        {id: 'course3', title: 'Example Course 3', quizzes: [1,2,4,5,6,7]}
+    ];
+
     return (
         <section class="section">
         <AuthCheck />
         <div class="container">
           <h1 class="title">Courses</h1>
           <hr />
-          <div class="tile is-ancestor">
+          <div class="tile is-ancestor" style={{flexWrap: "wrap"}}>
 
-            <div class="tile is-4 is-parent">
-                <Link to="/instructor/course/1" className="tile is-child box">
+          <div class="tile is-4 is-parent">
+                <span className="tile is-child box">
                     <p class="title">
-                        <span class="icon"><i class="fas fa-flask" aria-hidden="true"></i></span>
+                        <span class="icon"><i class="fas fa-plus-square" aria-hidden="true"></i></span>
                         &nbsp;
-                        Not a Real Course
+                        Create Course
                     </p>
-                    <hr />
                     <div class="content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                        <br />
-                        5 Quizzes
+                        <CreateCourseForm />
                     </div>
-                </Link>
+                </span>
             </div>
 
-            <div class="tile is-4 is-parent">
-                <Link to="/instructor/course/1" className="tile is-child box">
-                    <p class="title">
-                        <span class="icon"><i class="fas fa-flask" aria-hidden="true"></i></span>
-                        &nbsp;
-                        Another Fake Course
-                    </p>
-                    <hr />
-                    <div class="content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                        <br />
-                        7 Quizzes
-                    </div>
-                </Link>
-            </div>
+            {courses.map((course, index) => 
+                <div class="tile is-4 is-parent">
+                    <Link to={"/instructor/course/" + course.id} className="tile is-child box">
+                        <p class="title">
+                            <span class="icon"><i class="fas fa-flask" aria-hidden="true"></i></span>
+                            &nbsp; &nbsp;
+                            {course.title}
+                        </p>
+                        <hr />
+                        <div class="content">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                            <br />
+                            {course.quizzes.length === 1 ? '1 Quiz' : course.quizzes.length + ' Quizzes'}
+                        </div>
+                    </Link>
+                </div>
+            )}
 
         </div>
 
---
+++ client/src/components/instructor/CreateCourseForm.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class CreateCourseForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newTitle: ''
+        };
+    }
+
+    _handleNewTitleChange(event) {
+        this.setState({ newTitle: event.target.value });
+    }
+
+    _handleNewTitleKeyPress(event) {
+        if (event.key === 'Enter') {
+            event.target.blur();
+            this._createNewCourse();
+        }
+    }
+
+    _createNewCourse() {
+        alert('Would create course: ' + this.state.newTitle);
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="field control">
+                    <input type="text"
+                        ref="newCourseTitle"
+                        value={this.state.newTitle}
+                        onChange={(e) => this._handleNewTitleChange(e)}
+                        onKeyPress={(e) => this._handleNewTitleKeyPress(e)}
+                        className="input"
+                        placeholder="New Course Title" />
+                </div>
+                <div className="field control">
+                    <button className="button is-primary" onClick={() => this._createNewCourse()}>Create</button>
+                </div>
+            </div>
+        );
+    }
+}
