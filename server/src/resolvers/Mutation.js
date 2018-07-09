ec977600944364d3168cf815f41f47888113623f
Robert Nay <rsn.nay@gmail.com>
2018-07-09 17:11:14 -0600

    Backend: mutations for logging in and signing up

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -1,3 +1,8 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { APP_SECRET } = require('../../config');
+const { validateEmail } = require('../utils');
+
 function updateOption (root, args, context, info){
     return context.db.mutation.updateOption({
         data:{
@@ -207,6 +212,57 @@ function updateQuestion(root, args, context, info){
     }, info)
 }
 
+async function instructorLogin(root, args, context, info) {
+    // Check that user exists
+    console.log(args.email, args.password);
+    const email = args.email.toLowerCase();
+    const user = await context.db.query.user({ where: { email: email } }, ` { id, password } `);
+    if (!user) {
+        throw new Error('Invalid email and/or password');
+    }
+
+    // Check that password is valid
+    const valid = await bcrypt.compare(args.password, user.password);
+    if (!valid) {
+        throw new Error('Invalid email and/or password');
+    }
+
+    // Sign token
+    const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+    // Return an AuthPayload
+    return {
+        token,
+        user,
+    };
+}
+
+async function instructorSignup(root, args, context, info) {
+    // Lower-case email
+    const email = args.email.toLowerCase();
+    // Validate email
+    if (!validateEmail(email)) {
+        throw new Error('Invalid email address');
+    }
+    // Hash password
+    const password = await bcrypt.hash(args.password, 10);
+    // Signup is only for instructors (student accounts will be auto-created when launched via LTI)
+    const role = "instructor";
+    // Create user
+    const user = await context.db.mutation.createUser({
+        data: { email, password, role },
+    }, `{ id }`);
+
+    // Sign token using id of newly-created user
+    const token = jwt.sign({ userId: user.id }, APP_SECRET);
+
+    // Return an AuthPayload
+    return {
+        token,
+        user,
+    };
+}
+
 module.exports = {
     addQuiz,
     addUser,
@@ -216,5 +272,7 @@ module.exports = {
     addQuestion,
     updateQuestion,
     deleteQuestion,
-    updateOption
+    updateOption,
+    instructorLogin,
+    instructorSignup
 }
