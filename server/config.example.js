ee1bfa97c34cc5b4a93ba08d25cc6efbc3c34a4b
Robert Nay <rsn.nay@gmail.com>
2018-07-16 12:29:20 -0600

    Server config and schema changes

--- server/config.example.js
+++ server/config.example.js
@@ -1,11 +1,14 @@
 // Copy this file and rename it to config.js, and replace the variables with your configuration
 const APP_SECRET = 'SECRET';
 
+const APP_SERVER_PORT = 4000;
+
 const PRISMA_ENDPOINT = 'ENDPOINT';
 const PRISMA_SECRET = 'SECRET';
 
 module.exports = {
     APP_SECRET,
+    APP_SERVER_PORT,
     PRISMA_ENDPOINT,
     PRISMA_SECRET
 };
