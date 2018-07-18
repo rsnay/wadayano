1b4f0ef2715fe9a80233923717f65271a6c91050
Robert Nay <rsn.nay@gmail.com>
2018-07-18 15:54:19 -0600

    Add getUserInfo function to return user ID and if user is instructor or not

--- server/src/utils.js
+++ server/src/utils.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const {APP_SECRET } = require('../config');
 
 function validateEmail(email) {
@@ -9,14 +10,34 @@ function getUserId(context) {
     const Authorization = context.request.get('Authorization')
     if (Authorization) {
       const token = Authorization.replace('Bearer ', '')
-      const { instructorId } = jwt.verify(token, APP_SECRET)
-      return instructorId
+      const { userId } = jwt.verify(token, APP_SECRET)
+      return userId
     }
   
     throw new Error('Not authenticated')
-  }
+}
+
+// Extracts user ID and role (instructor or not) from the JWT. Returns { userId: "string", isInstructor: true/false }
+function getUserInfo(context) {
+    const Authorization = context.request.get('Authorization');
+    if (Authorization) {
+      const token = Authorization.replace('Bearer ', '');
+      try {
+        const { userId, isInstructor } = jwt.verify(token, APP_SECRET);
+        return { userId, isInstructor };
+      } catch (error) {
+        // TODO if not authenticated or invalid token, should this return empty values, or throw an error?
+        //return { userId: null, isInstructor: false };
+        throw new Error('Not authenticated');
+      }
+    }
+  
+    //return { userId: null, isInstructor: false };
+    throw new Error('Not authenticated');
+}
 
 module.exports = {
     validateEmail,
-    getUserId
+    getUserId,
+    getUserInfo
 };
