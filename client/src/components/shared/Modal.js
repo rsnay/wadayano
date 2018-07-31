817a329a5c6428b1161adfc2b98acab86731a5f5
Robert Nay <rsn.nay@gmail.com>
2018-07-30 19:55:55 -0600

    Add generic modal dialog component

---
+++ client/src/components/shared/Modal.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = ({ children, closeModal, modalState, title, showFooter }) => {
+    if(!modalState) {
+      return null;
+    }
+    
+    return(
+      <div className="modal is-active">
+        <div className="modal-background" onClick={closeModal} />
+        <div className="modal-card">
+          <header className="modal-card-head">
+            <p className="modal-card-title">{title}</p>
+            <button className="delete" onClick={closeModal} />
+          </header>
+          <section className="modal-card-body">
+            <div className="content">
+              {children}
+            </div>
+          </section>
+          {showFooter && <footer className="modal-card-foot">
+            <a className="button" onClick={closeModal}>Done</a>
+          </footer>}
+        </div>
+      </div>
+    );
+}
+  
+Modal.propTypes = {
+    closeModal: PropTypes.func.isRequired,
+    modalState: PropTypes.bool.isRequired,
+    title: PropTypes.string,
+    showFooter: PropTypes.bool
+}
+
+export default Modal;
