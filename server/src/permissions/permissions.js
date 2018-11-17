0cf590c9eabc9b9fb705e49b2a0e1b931a772044
Robert Nay <rsn.nay@gmail.com>
2018-11-17 12:07:10 -0700

    Continue adding rules and permissions for backend schema

--- server/src/permissions/permissions.js
+++ server/src/permissions/permissions.js
@@ -1,14 +1,45 @@
-const { shield, and, or } = require('graphql-shield');
+const { shield, and, or, allow, deny } = require('graphql-shield');
 const rules = require('./rules');
 
 // Restrict most things in the public-facing schema.graphql (which includes some things from the internal datamodel.graphql)
 // Currently, isCorrect on Option is protected from student access via a custom resolver
 const Permissions = {
-    Query: {  
-        //course: rules.canAccessCourse,
-        //courses: rules.canAccessCourse
+    Query: {
+        quiz: rules.canAccessQuiz,
+        currentInstructor: rules.isInstructor,
+        course: rules.isAuthenticated, // More lax here, since individual properties are controlled below
+        question: rules.canAccessQuestion,
+        currentStudent: rules.isStudent,
+        currentStudentQuizAttempts: rules.isStudent,
+        quizAttempt: rules.canAccessQuizAttempt
     },
     Mutation: {
+        updateQuiz: rules.ownsQuiz,
+        addCourse: rules.isInstructor,
+        createQuiz: rules.ownsCourse,
+        updateCourse: rules.ownsCourse,
+        deleteCourse: rules.ownsCourse,
+        deleteQuiz: rules.ownsQuiz,
+        addQuestion: rules.ownsQuiz,
+        updateQuestion: rules.ownsQuestion,
+        deleteQuestion: rules.ownsQuestion,
+        importQuestions: rules.ownsQuiz,
+        updateSurvey: rules.ownsCourse,
+        sendInstructorCourseInvite: rules.ownsCourse,
+        removeInstructorFromCourse: rules.ownsCourse,
+      
+        instructorSignup: allow,
+        instructorLogin: allow,
+        instructorRequestPasswordReset: allow,
+        instructorResetPassword: allow,
+        sendFeedback: rules.isAuthenticated,
+      
+        startOrResumeQuizAttempt: rules.enrolledInQuiz,
+        // completeQuizAttempt: rules.ownsQuizAttempt,
+        // rateConcepts: rules.ownsQuizAttempt,
+        // attemptQuestion: rules.ownsQuizAttempt,
+      
+        submitSurveyResult: rules.enrolledInCourse
     },
     Course: {
         title: rules.canAccessCourse,
@@ -22,11 +53,16 @@ const Permissions = {
         surveyResults: rules.ownsCourse,
         pendingCourseInvites: rules.ownsCourse
     },
-    Question: {
-        options: rules.canAccessQuiz,
-        prompt: rules.canAccessQuiz,
-        concept: rules.canAccessQuiz
+    // No additional controls on quiz or question are needed, since if a user can access a course, they have permission to access all quizzes and questions within the course
+    // Exceptions to above comment: restrict backlink of Quiz to QuizAttempts, and Option to QuestionAttempts
+    Quiz: {
+        quizAttempts: rules.isInstructor
     },
+    Option: {
+        // These are needed as backlinks (see comment in datamodel.graphql), but no user should ever access them
+        questionAttempts: deny,
+        correctQuestionAttempts: deny
+    }
 
 };
 
--- server/src/permissions/rules.js
+++ server/src/permissions/rules.js
@@ -1,13 +1,18 @@
 const { rule, and, or, not } = require('graphql-shield');
 const { getUserInfo } = require('../utils');
 
-const isStudent = rule()(async (parent, args, context, info) => {
+// Each of these rules returns a boolean to allow/deny access
+// Rules that use the `context` parameter (any that use getUserInfo) need to have the cache property set to contextual.
+// Rules that use `parent` or `args` parameters (checking if a user can access a particular entity) need to have cache set to strict
+// Some rules that check for permission on a specific DB entity need to get the entity ID either from the parent (if part of a resolves), or from args (if the rule is attached to a Query in the schema)
+
+const isStudent = rule({ cache: 'contextual' })(async (parent, args, context, info) => {
     console.log('SHIELD: IsStudent?');
     const userInfo = getUserInfo(context);
     return (userInfo.userId !== null) && !userInfo.isInstructor;
 });
 
-const isInstructor = rule()(async (parent, args, context, info) => {
+const isInstructor = rule({ cache: 'contextual' })(async (parent, args, context, info) => {
     console.log('SHIELD: IsInstructor?');
     const userInfo = getUserInfo(context);
     return (userInfo.userId !== null) && userInfo.isInstructor;
@@ -15,14 +20,26 @@ const isInstructor = rule()(async (parent, args, context, info) => {
 
 const isAuthenticated = or(isStudent, isInstructor);
 
-const ownsCourse = rule({ cache: 'strict', fragment: 'fragment CourseId on Course { id }' })((parent, args, context, info) => {
-    // TODO: integrate fragment replacement middleware so that ID will always be in parent
-    if (!parent.id) { return false; }
-    const userInfo = getUserInfo(context);
+// TODO: integrate fragment replacement middleware so that ID will always be in parent
+
+const ownsCourse = rule({
+    cache: 'strict',
+    fragment: 'fragment CourseId on Course { id }'
+}) ((parent, args, context, info) => {
     console.log('SHIELD: OwnsCourse?');
+    // ID to check could be parent object (from resolver), or `id` or similar argument from query
+    let id = null;
+    if (parent && parent.id) {
+        id = parent.id;
+    } else if (args) {
+        id = args.id || args.courseId;
+    }
+    if (!id) { return false; }
+
+    const userInfo = getUserInfo(context);
 
     return userInfo.isInstructor && context.db.exists.Course({
-        id: parent.id,
+        id: id,
         instructors_some: { 
             id: userInfo.userId
         }
@@ -30,13 +47,20 @@ const ownsCourse = rule({ cache: 'strict', fragment: 'fragment CourseId on Cours
 });
 
 const enrolledInCourse = rule({ cache: 'strict', fragment: 'fragment CourseId on Course { id }' })((parent, args, context, info) => {
-    // TODO: integrate fragment replacement middleware so that ID will always be in parent
-    if (!parent.id) { return false; }
-    const userInfo = getUserInfo(context);
     console.log('SHIELD: EnrolledInCourse?');
+    // ID to check could be parent object (from resolver), or `id` or similar argument from query
+    let id = null;
+    if (parent && parent.id) {
+        id = parent.id;
+    } else if (args) {
+        id = args.id || args.courseId;
+    }
+    if (!id) { return false; }
+
+    const userInfo = getUserInfo(context);
 
     return (!userInfo.isInstructor) && context.db.exists.Course({
-        id: parent.id,
+        id: id,
         students_some: { 
             id: userInfo.userId
         }
@@ -45,28 +69,132 @@ const enrolledInCourse = rule({ cache: 'strict', fragment: 'fragment CourseId on
 
 const canAccessCourse = or(enrolledInCourse, ownsCourse);
 
-const canAccessQuiz = rule({ cache: 'strict', fragment: 'fragment QuizId on Quiz { id }' })((parent, args, context, info) => {
-    // TODO actual implementation
-    console.log('SHIELD: CanAccessQuiz?');
-    return true;
-    // TODO: integrate fragment replacement middleware so that ID will always be in parent
-    if (!parent.id) { return false; }
+const ownsQuiz = rule({
+    cache: 'strict',
+    fragment: 'fragment QuizId on Quiz { id }'
+}) ((parent, args, context, info) => {
+    console.log('SHIELD: ownsQuiz?', args);
+    // ID to check could be parent object (from resolver), or `id` or similar argument from query
+    let id = null;
+    if (parent && parent.id) {
+        id = parent.id;
+    } else if (args) {
+        id = args.id || args.quizId;
+    }
+    if (!id) { return false; }
+
     const userInfo = getUserInfo(context);
 
-    return (!userInfo.isInstructor) && context.db.exists.Course({
-        id: parent.id,
-        students_some: { 
-            id: userInfo.userId
+    return (userInfo.isInstructor && context.db.exists.Quiz({
+        id: id,
+        course: {
+            instructors_some: { 
+                id: userInfo.userId
+            }
+        }
+    }));
+});
+
+const enrolledInQuiz = rule({
+    cache: 'strict',
+    fragment: 'fragment QuizId on Quiz { id }'
+}) ((parent, args, context, info) => {
+    console.log('SHIELD: enrolledInQuiz?');
+    // Quiz to check could be parent object (from resolver), or `id` or `quizId` argument from query
+    let id = null;
+    if (parent && parent.id) {
+        id = parent.id;
+    } else if (args) {
+        id = args.id || args.quizId;
+    }
+    if (!id) { return false; }
+
+    const userInfo = getUserInfo(context);
+
+    return (!userInfo.isInstructor) && context.db.exists.Quiz({
+        id: id,
+        course: {
+            students_some: { 
+                id: userInfo.userId
+            }
         }
     });
 });
+
+const canAccessQuiz = or(enrolledInQuiz, ownsQuiz);
+
+const ownsQuestion = rule({
+    cache: 'strict',
+    fragment: 'fragment QuestionId on Question { id }'
+}) ((parent, args, context, info) => {
+    console.log('SHIELD: ownsQuestion?');
+    // Question to check could be parent object (from resolver), or `id` or `questionId` argument from query
+    let id = null;
+    if (parent && parent.id) {
+        id = parent.id;
+    } else if (args) {
+        id = args.id || args.questionId;
+    }
+    if (!id) { return false; }
+
+    const userInfo = getUserInfo(context);
+
+    return (userInfo.isInstructor && context.db.exists.Course({
+        quizzes_some: {
+            questions_some: {
+                id: id
+            }
+        },
+        instructors_some: { 
+            id: userInfo.userId
+        }
+    }));
+});
+
+const enrolledInQuestion = rule({
+    cache: 'strict',
+    fragment: 'fragment QuestionId on Question { id }'
+}) ((parent, args, context, info) => {
+    console.log('SHIELD: enrolledInQuestion?');
+    // Question to check could be parent object (from resolver), or `id` or `questionId` argument from query
+    let id = null;
+    if (parent && parent.id) {
+        id = parent.id;
+    } else if (args) {
+        id = args.id || args.questionId;
+    }
+    if (!id) { return false; }
+
+    const userInfo = getUserInfo(context);
+
+    return (!userInfo.isInstructor && context.db.exists.Course({
+        quizzes_some: {
+            questions_some: {
+                id: id
+            }
+        },
+        students_some: { 
+            id: userInfo.userId
+        }
+    }));
+});
+
+const canAccessQuestion = or(enrolledInQuestion, ownsQuestion);
 
 module.exports = {
     isStudent,
     isInstructor,
     isAuthenticated,
+
     ownsCourse,
     enrolledInCourse,
     canAccessCourse,
-    canAccessQuiz
+
+    ownsQuiz,
+    enrolledInQuiz,
+    canAccessQuiz,
+
+    ownsQuestion,
+    enrolledInQuestion,
+    canAccessQuestion
 };
