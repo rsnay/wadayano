c65eb2093b29e3e0a943d19ce5c3fc3c28f53abc
Robert Nay <rsn.nay@gmail.com>
2019-02-06 12:30:54 -0700

    Allow props to control LoadingBox container

--- client/src/components/shared/LoadingBox.js
+++ client/src/components/shared/LoadingBox.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export default class LoadingBox extends Component {
     render() {
         return (
-            <div className="container section">
+            <div className="container section" {...this.props}>
                 <center>
                     <div className="button is-large is-primary is-loading">Loading</div>
                     {this.props.children}
