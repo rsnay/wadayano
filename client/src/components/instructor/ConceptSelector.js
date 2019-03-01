fc35629a924cdde951bc9ec06f2442f6c3d700b8
Robert Nay <rsn.nay@gmail.com>
2019-03-01 09:43:01 -0700

    Concept selector cleanup, and show suggestions when focusing, not just on change

--- client/src/components/instructor/ConceptSelector.js
+++ client/src/components/instructor/ConceptSelector.js
@@ -3,30 +3,30 @@ import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-import { withAuthCheck } from '../shared/AuthCheck';
+const SUGGESTION_COUNT = 10;
 
 class ConceptSelector extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            concept: "",
             showSuggestions: false,
             concepts: []
         };
         this.handleInputChange = this.handleInputChange.bind(this);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({ concept: nextProps.concept })
+        this.handleFocus = this.handleFocus.bind(this);
     }
 
     handleInputChange(event) {
         this.setConcept(event.target.value, true);
     }
 
+    handleFocus() {
+        this.setState({ showSuggestions: true });
+    }
+
     setConcept(concept, showSuggestions = false) {
-        this.setState({ concept, showSuggestions });
+        this.setState({ showSuggestions });
         if (this.props.onChange) {
             this.props.onChange(concept);
         }
@@ -34,21 +34,21 @@ class ConceptSelector extends Component {
 
     render() {
         // Prepare filtering (non-lowercase is used in display)
-        const filterText = this.state.concept.trim();
+        const filterText = this.props.concept.trim();
         const lowerCaseFilterText = filterText.toLowerCase();
         // Use course concepts, if loaded
-        const concepts = (this.props.courseQuery && !this.props.courseQuery.loading && !this.props.courseQuery.error && this.props.courseQuery.courseConcepts) ? this.props.courseQuery.courseConcepts : [];
+        const concepts = (this.props.conceptsQuery && !this.props.conceptsQuery.loading && !this.props.conceptsQuery.error && this.props.conceptsQuery.courseConcepts) ? this.props.conceptsQuery.courseConcepts : [];
         // Perform filtering
         let filteredConcepts = concepts.filter(concept => (concept.toLowerCase().includes(lowerCaseFilterText)));
 
         let suggestionsList = this.state.showSuggestions ? (
             <span className="concept-suggestions-list">
-            {filteredConcepts.slice(0,10).map(concept => (
+            {filteredConcepts.slice(0, SUGGESTION_COUNT).map(concept => (
                 <button key={concept} className="concept-tag tag is-light" onClick={() => this.setConcept(concept, false)}>{concept}</button>
             ))}
             {/* Add current filter text, if not empty or already present */}
             {(filterText !== '' && filteredConcepts.indexOf(filterText) === -1) && 
-                <button key={filterText} className="concept-tag tag is-link" onClick={() => this.setConcept(filterText, false)}>+ {filterText}</button>
+                <button className="concept-tag tag is-link" onClick={() => this.setConcept(filterText, false)}>+ {filterText}</button>
             }
             </span>
         ) : (<span className="concept-suggestions-list"></span>);
@@ -56,7 +56,8 @@ class ConceptSelector extends Component {
         return (
             <React.Fragment>
                 <input
-                    value={this.state.concept}
+                    value={this.props.concept}
+                    onFocus={this.handleFocus}
                     onChange={this.handleInputChange}
                     className="input is-inline"
                     type="text"
@@ -82,17 +83,15 @@ ConceptSelector.defaultProps = {
 };
 
 // Get the concepts from the whole course
-const COURSE_CONCEPTS_QUERY = gql`
+const CONCEPTS_QUERY = gql`
   query courseConceptsQuery($id: ID!) {
     courseConcepts(id:$id)
   }
-`
+`;
 
-export default withAuthCheck(
-    graphql(COURSE_CONCEPTS_QUERY, {
-        name: 'courseQuery',
-        options: (props) => {
-            return { variables: { id: props.courseId } }
-        }
+export default graphql(CONCEPTS_QUERY, {
+    name: 'conceptsQuery',
+    options: (props) => {
+        return { variables: { id: props.courseId } }
     }
-) (ConceptSelector), { instructor: true });
+}) (ConceptSelector);
