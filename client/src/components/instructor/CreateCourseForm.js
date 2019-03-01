645a8e539703bce12fd091b2f448753355758b1a
Robert Nay <rsn.nay@gmail.com>
2019-03-01 09:42:25 -0700

    Prevent duplicate submission on create new course form

--- client/src/components/instructor/CreateCourseForm.js
+++ client/src/components/instructor/CreateCourseForm.js
@@ -5,27 +5,32 @@ import gql from 'graphql-tag';
 
 import ButterToast, { ToastTemplate } from '../shared/Toast';
 
+/**
+ * Card with a simple form to create a new course.
+ * Intended for use in CourseList.
+ */
 export class CreateCourseForm extends Component {
     constructor(props) {
         super(props);
         this.state = {
             newTitle: '',
+            isCreating: false,
             redirectCourseId: null
         };
     }
 
-    _handleNewTitleChange(event) {
+    handleNewTitleChange(event) {
         this.setState({ newTitle: event.target.value });
     }
 
-    _handleNewTitleKeyPress(event) {
-        if (event.key === 'Enter') {
+    handleNewTitleKeyPress(event) {
+        if (event.key === 'Enter' && !this.state.isCreating) {
             event.target.blur();
-            this._createNewCourse();
+            this.createNewCourse();
         }
     }
 
-    async _createNewCourse() {
+    async createNewCourse() {
         // Make sure there is a non-empty course title
         if (this.state.newTitle.trim() === '') {
             ButterToast.raise({
@@ -34,6 +39,7 @@ export class CreateCourseForm extends Component {
             });
             return;
         }
+        this.setState({ isCreating: true });
         // Create the course
         let result = await this.props.addCourseMutation({
             variables: {
@@ -41,7 +47,7 @@ export class CreateCourseForm extends Component {
             }
         });
         // Redirect to the new course
-        this.setState({ redirectCourseId: result.data.addCourse.id });
+        this.setState({ redirectCourseId: result.data.addCourse.id, isCreating: false });
     }
 
     render() {
@@ -57,14 +63,14 @@ export class CreateCourseForm extends Component {
                 <div className="field control">
                     <input type="text"
                         value={this.state.newTitle}
-                        onChange={(e) => this._handleNewTitleChange(e)}
-                        onKeyPress={(e) => this._handleNewTitleKeyPress(e)}
+                        onChange={(e) => this.handleNewTitleChange(e)}
+                        onKeyPress={(e) => this.handleNewTitleKeyPress(e)}
                         className="input"
                         maxLength={200}
                         placeholder="New Course Title" />
                 </div>
                 <div className="field control">
-                    <button className="button is-primary" onClick={() => this._createNewCourse()}>Create</button>
+                    <button className="button is-primary" onClick={() => this.createNewCourse()} disabled={this.state.isCreating}>Create</button>
                 </div>
             </div>
         );
