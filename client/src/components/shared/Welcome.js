c47a89b3cf3efc4f10b5782c5fea313a912decd4
Robert Nay <rsn.nay@gmail.com>
2019-03-08 15:45:28 -0700

    Code cleanup: comments and stylistic improvements on welcome page

--- client/src/components/shared/Welcome.js
+++ client/src/components/shared/Welcome.js
@@ -3,46 +3,51 @@ import { Link } from 'react-router-dom';
 
 import { AUTH_ROLE, AUTH_ROLE_STUDENT, AUTH_ROLE_INSTRUCTOR } from '../../constants';
 
-export default class Welcome extends Component {
-
-  componentDidMount() {
-    const authRole = localStorage.getItem(AUTH_ROLE);
-    if (authRole === AUTH_ROLE_STUDENT) {
-      this.props.history.replace('/student/dashboard');
-      return;
+/**
+ * The page displayed at the root path (e.g. https://wadayano.com/ )
+ * Redirects to the student or instructor dashboard, or offers login/signup options
+ */
+ export default class Welcome extends Component {
+    
+    componentDidMount() {
+        const authRole = localStorage.getItem(AUTH_ROLE);
+        if (authRole === AUTH_ROLE_STUDENT) {
+            this.props.history.replace('/student/dashboard');
+            return;
+        }
+        if (authRole === AUTH_ROLE_INSTRUCTOR) {
+            this.props.history.replace('/instructor/courses');
+            return;
+        }
     }
-    if (authRole === AUTH_ROLE_INSTRUCTOR) {
-      this.props.history.replace('/instructor/courses');
-      return;
+
+    render() {
+
+        return (
+            <section className="section">
+                <div className="container">
+                    <h1 className="title">Welcome to wadayano!</h1>
+                    <div className="content">
+                        <p>To get started, log in or sign up.</p>
+
+                        <p className="buttons">
+                            <Link to="/login" className="button is-primary">
+                                <span className="icon">
+                                    <i className="fas fa-sign-in-alt"></i>
+                                </span>
+                                <span>Log In</span>
+                            </Link>
+                            <Link to="/signup" className="button is-primary">
+                                <span className="icon">
+                                    <i className="fas fa-user-plus"></i>
+                                </span>
+                                <span>Sign Up</span>
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </section>
+        );
     }
-  }
-
-  render() {
-
-    return (
-        <section className="section">
-        <div className="container">
-          <h1 className="title">Welcome to wadayano!</h1>
-          <div className="content">
-            <p>To get started, log in or sign up.</p>
-            <p className="buttons">
-              <Link to="/login" className="button is-primary">
-                <span className="icon">
-                  <i className="fas fa-sign-in-alt"></i>
-                </span>
-                <span>Log In</span>
-              </Link>
-              <Link to="/signup" className="button is-primary">
-                <span className="icon">
-                  <i className="fas fa-user-plus"></i>
-                </span>
-                <span>Sign Up</span>
-              </Link>
-            </p>
-          </div>
-        </div>
-      </section>
-    )
-  }
 
 }
