038211d88611b0204ef66e8f0b17303ff2f00dbe
Robert Nay <rsn.nay@gmail.com>
2019-04-26 17:22:51 -0600

    Finish profile editor, supporting changing email address and password

--- client/src/components/instructor/ProfileEditor.js
+++ client/src/components/instructor/ProfileEditor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -7,52 +7,104 @@ import { withAuthCheck } from '../shared/AuthCheck';
 
 import ErrorBox from '../shared/ErrorBox';
 import LoadingBox from '../shared/LoadingBox';
+import ButterToast, { ToastTemplate } from '../shared/Toast';
 
 class ProfileEditor extends Component {
+
     constructor(props) {
         super(props);
-
+        
         this.state = {
             instructorLoaded: false,
-            isSaving: false
+            isSaving: false,
+            shouldRedirect: false,
+            currentPassword: '',
+            newEmail: '',
+            newPassword: '',
+            newPasswordConfirm: '',
+            error: ''
         };
+        
+        // Pre-bind this function, to make adding it to input fields easier
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
     // This is kind of a react anti-pattern to seed state from props, but without a proper callback for when the apollo query finishes loading, it's the easiest way.
     componentWillReceiveProps(nextProps) {
-        if (!nextProps.instructorQuery.loading && !this.state.instructorLoaded) {
-            // TODO
-            /*this.setState({
-                newSurveyText: this._stringifySurvey(nextProps.courseQuery.course.survey),
-                surveyLoaded: true
-            });*/
+        console.log(nextProps);
+        if (!nextProps.instructorQuery.error && !nextProps.instructorQuery.loading && !this.state.instructorLoaded) {
+            this.setState({
+                newEmail: nextProps.instructorQuery.currentInstructor.email,
+                instructorLoaded: true
+            });
         }
     }
 
-    // If navigating away from this component and back to it, apollo won't reload the query, but automatically populate the courseQuery prop. So handle that case on component mount.
-    componentDidMount() {
-        // TODO
-        /*if (!this.state.surveyLoaded && this.props.courseQuery && !this.props.courseQuery.loading && this.props.courseQuery.course.survey) {
-            this.setState({
-                newSurveyText: this._stringifySurvey(this.props.courseQuery.course.survey),
-                surveyLoaded: true
-            });
-        }*/
+    // When the update button is pressed or form is submitted via enter key
+    submit(e) {
+        if (e) {
+            e.preventDefault();
+        }
+        // Prevent re-submission while loading
+        if (this.state.isSaving) { return; }
+        this.updateProfile();
     }
 
-    async _saveSurvey() {
-        this.setState({ isSaving: true });
+    // Update the instructor’s email address and/or password
+    async updateProfile() {
+        this.setState({ error: '', isSaving: true });
+        // Check that passwords match
+        if (this.state.newPassword.length > 0 && (this.state.newPassword !== this.state.newPasswordConfirm)) {
+            this.setState({ error: 'New passwords do not match.', isLoading: false });
+            return;
+        }
+        
+        // Send update profile mutation
+        const id = this.props.instructorQuery.currentInstructor.id;
+        const { currentPassword, newEmail, newPassword } = this.state;
         try {
-            await this.props.saveSurveyMutation({
+            const result = await this.props.instructorUpdateProfileMutation({
                 variables: {
-                    courseId: this.props.courseQuery.course.id,
-                    survey: this._parseSurveyText(this.state.newSurveyText)
+                    id,
+                    currentPassword,
+                    newEmail,
+                    newPassword
                 }
             });
-        } catch (error) {
-            alert('There was an error saving the survey. Please copy the text somewhere safe and try again later.');
+            if (result.errors && result.errors.length > 0) {
+                throw result;
+            }
+            console.log("Update result", result);
+            // Show success message
+            this.setState({ isSaving: false, shouldRedirect: true }, () => {
+                ButterToast.raise({
+                    content: <ToastTemplate content="Your profile was updated." className="is-success" />
+                });
+            });
+        } catch (e) {
+            let message = 'Please try again later.';
+            if (e.errors && e.errors.length > 0) {
+                message = e.errors[0].message;
+            }
+            // Decode a specific error message
+            if (message.indexOf('unique constraint would be violated') >= 0) {
+                message = 'An existing instructor already has the given new email address.';
+            }
+            this.setState({ error: 'Error updating profile: ' + message, isSaving: false });
+            console.error('Error updating profile: ' + JSON.stringify(e));
         }
-        this.setState({ isSaving: false });
+    }
+
+    // Called when the form fields change
+    // This function is from https://reactjs.org/docs/forms.html
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        
+        this.setState({
+            [name]: value
+        });
     }
 
     render() {
@@ -62,43 +114,126 @@ class ProfileEditor extends Component {
         }
 
         if (this.props.instructorQuery && this.props.instructorQuery.error) {
-            return <ErrorBox><p>Couldn’t load instructor profile</p></ErrorBox>;
+            return <ErrorBox><p>Error loading profile. Please try again later.</p></ErrorBox>;
         }
 
-        const instructor = this.props.instructorQuery.currentInstructor;
+        // Redirect to course list after successful update
+        // This is necessary to trigger the browser prompting to save the updated password
+        if(this.state.shouldRedirect) {
+            return <Redirect push to="/instructor" />;
+        }
+
+        const { currentPassword, newEmail, newPassword, newPasswordConfirm } = this.state;
+
+        const formCompleted = currentPassword && ((newPassword || newPasswordConfirm) ? 
+                                                    (newPasswordConfirm === newPassword) : true);
 
         return (
-            <section className="section">
-                <div className="container">
+        <section className="section no-select container">
+            <h1 className="title is-3">My Profile</h1>
+            <i>Enter your current password to change your email address or set a new password.</i>
+            <br />
+            <i>Leave new password or email blank to keep existing password/email.</i>
 
-                    <h3 className="title is-3">My Profile</h3>
-                    <hr /><br />
+            <form className="column is-one-third-desktop is-half-tablet" onSubmit={(e) => this.submit(e) }>
 
-                    {/* TODO form to edit the profile */}
-                    <pre>{JSON.stringify(instructor)}</pre>
+                {this.state.error && <p className="notification is-danger">{this.state.error}</p> }
 
-                    <br /> <br />
-                    <div className="field is-grouped">
-                        <p className="control">
-                            <Link className="button" to="/instructor/courses">Cancel</Link>
-                        </p>
-                        <p className="control">
-                            <button
-                                className={"button is-primary" + (this.state.isSaving ? " is-loading" : "")}
-                                onClick={() => alert('Not yet implemented')}>
-                                Update Profile
-                            </button>
-                        </p>
-                    </div>
-
-
+                <div className="field">
+                    <label class="label">Current Password</label>
+                    <p className="control has-icons-left">
+                        <input
+                            required
+                            value={currentPassword}
+                            name="currentPassword"
+                            onChange={this.handleInputChange}
+                            className="input"
+                            type="password"
+                            placeholder="Current Password"
+                        />
+                        <span className="icon is-small is-left">
+                        <i className="fas fa-lock"></i>
+                        </span>
+                    </p>
                 </div>
-            </section>
+
+                <div className="field">
+                    <label class="label">Email Address</label>
+                    <p className="control has-icons-left has-icons-right">
+                        <input
+                            value={newEmail}
+                            name="newEmail"
+                            onChange={this.handleInputChange}
+                            className="input"
+                            type="email"
+                            maxLength={255}
+                            placeholder="Email Address"
+                        />
+                        <span className="icon is-small is-left">
+                        <i className="fas fa-envelope"></i>
+                        </span>
+                    </p>
+                </div>
+
+                <div className="field">
+                    <label class="label">New Password</label>
+                    <p className="control has-icons-left">
+                        <input
+                            value={newPassword}
+                            name="newPassword"
+                            onChange={this.handleInputChange}
+                            className="input"
+                            type="password"
+                            placeholder="New Password"
+                        />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-lock"></i>
+                        </span>
+                    </p>
+                </div>
+
+                <div className="field">
+                    <p className="control has-icons-left">
+                        <input
+                            value={newPasswordConfirm}
+                            name="newPasswordConfirm"
+                            onChange={this.handleInputChange}
+                            className="input"
+                            type="password"
+                            placeholder="Confirm New Password"
+                        />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-lock"></i>
+                        </span>
+                    </p>
+                </div>
+
+                <div className="field">
+                    <p className="control">
+                        <button
+                            className={"button is-primary" + (this.state.isSaving ? " is-loading" : "")}
+                            disabled={!formCompleted || this.state.isSaving}
+                            onClick={() => this.submit() }
+                        >
+                            Update Profile
+                        </button>
+                    </p>
+                </div>
+            </form>
+
+            <br />
+
+            <h2 className="subtitle is-4">Delete My Account</h2>
+            <p className="content">If you need to delete your instructor account, please <Link to="/feedback">contact us</Link>.</p>
+
+            {/*<pre>{JSON.stringify(instructor)}</pre>*/}
+
+        </section>
         );
     }
 }
 
-// Get the instructor’s profile
+// Get the instructor’s profile, so that email address can be pre-filled
 const INSTRUCTOR_QUERY = gql`
   query instructorQuery {
     currentInstructor {
@@ -110,24 +245,14 @@ const INSTRUCTOR_QUERY = gql`
         }
     }
   }
-`
+`;
 
-// TODO
-const SAVE_INSTRUCTOR = gql`
-mutation saveInstructorMutation(
-    $courseId:ID!
-    $survey:Json!
-){
-    updateSurvey(
-        courseId: $courseId
-        survey: $survey
-    ){
-        id
-        survey
-    }
-}`
+const INSTRUCTOR_UPDATE_PROFILE_MUTATION = gql`
+mutation instructorUpdateProfileMutation ($id: ID!, $currentPassword: String!, $newEmail: String, $newPassword: String) {
+    instructorUpdateProfile(id: $id, currentPassword: $currentPassword, newEmail: $newEmail, newPassword: $newPassword)
+}`;
 
 export default withAuthCheck(compose(
     graphql(INSTRUCTOR_QUERY, { name: 'instructorQuery' }),
-    graphql(SAVE_INSTRUCTOR, {name: 'saveInstructorMutation'}),
+    graphql(INSTRUCTOR_UPDATE_PROFILE_MUTATION, { name: 'instructorUpdateProfileMutation' })
 ) (ProfileEditor), { instructor: true });
