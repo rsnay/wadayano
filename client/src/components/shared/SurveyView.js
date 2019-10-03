aab2400a2dfd95be8b8ee92e4679d116b64e6306
Robert Nay <rsn.nay@gmail.com>
2019-10-02 21:28:53 -0600

    Switch SurveyView to functional component

--- client/src/components/shared/SurveyView.js
+++ client/src/components/shared/SurveyView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 /**
@@ -6,63 +6,68 @@ import PropTypes from 'prop-types';
  * or taking (the parent component must provide the selected answers to and
  * receive changes from this component)
  */
-export default class SurveyView extends Component {
+const SurveyView = ({ survey, selectedAnswers = {}, onChange }) => {
   // When an option is selected, pass up the change
-  handleOptionClick(questionIndex, optionIndex) {
-    if (this.props.onChange) {
-      const newAnswers = { ...this.props.selectedAnswers };
+  const handleOptionClick = (questionIndex, optionIndex) => {
+    if (onChange) {
+      const newAnswers = { ...selectedAnswers };
       newAnswers[questionIndex] = optionIndex;
-      this.props.onChange(newAnswers);
+      onChange(newAnswers);
     }
-  }
+  };
 
   // Display selected answers from props, and pass selected answers up.
-  render() {
-    // If there's no change handler, disable the radio buttons
-    const disabled = !this.props.onChange;
-    const selectedAnswers = this.props.selectedAnswers || {};
+  // If there's no change handler, disable the radio buttons
+  const disabled = !onChange;
 
-    if (
-      !this.props.survey ||
-      !this.props.survey.questions ||
-      this.props.survey.questions.length === 0
-    ) {
-      return (
-        <p className="survey-question-prompt notification">
-          There are no questions in this survey.
-        </p>
-      );
-    }
-
-    const questions = this.props.survey.questions.map(q => (
-      <div className="survey-question" key={q.index}>
-        <p className="survey-question-prompt notification">
-          {q.index}.&nbsp; {q.prompt}
-        </p>
-        <div className="control">
-          {q.options.map(o => (
-            <label className="survey-question-option" key={`${q.index}o${o.index}`}>
-              <input
-                type="radio"
-                name={`question${q.index}`}
-                value={o.index}
-                disabled={disabled}
-                checked={selectedAnswers[q.index] === o.index}
-                onChange={() => this.handleOptionClick(q.index, o.index)}
-              />
-              <span className="survey-question-option-text">{o.text}</span>
-            </label>
-          ))}
-        </div>
-      </div>
-    ));
-
-    return <div>{questions}</div>;
+  if (!survey || !survey.questions || survey.questions.length === 0) {
+    return (
+      <p className="survey-question-prompt notification">There are no questions in this survey.</p>
+    );
   }
-}
+
+  const questions = survey.questions.map(q => (
+    <div className="survey-question" key={q.index}>
+      <p className="survey-question-prompt notification">
+        {q.index}.&nbsp; {q.prompt}
+      </p>
+      <div className="control">
+        {q.options.map(o => (
+          <label className="survey-question-option" key={`${q.index}o${o.index}`}>
+            <input
+              type="radio"
+              name={`question${q.index}`}
+              value={o.index}
+              disabled={disabled}
+              checked={selectedAnswers[q.index] === o.index}
+              onChange={() => handleOptionClick(q.index, o.index)}
+            />
+            <span className="survey-question-option-text">{o.text}</span>
+          </label>
+        ))}
+      </div>
+    </div>
+  ));
+
+  return <div>{questions}</div>;
+};
 
 SurveyView.propTypes = {
-  survey: PropTypes.object.isRequired,
+  survey: PropTypes.shape({
+    questions: PropTypes.arrayOf(
+      PropTypes.shape({
+        index: PropTypes.number.isRequired,
+        options: PropTypes.arrayOf(
+          PropTypes.shape({
+            index: PropTypes.number.isRequired,
+            text: PropTypes.string.isRequired,
+          })
+        ),
+      })
+    ),
+  }).isRequired,
   selectedAnswers: PropTypes.object,
   onChange: PropTypes.func,
 };
+
+export default SurveyView;
