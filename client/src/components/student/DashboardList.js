1e3b8b6f71982eaecf9e468b3f93b5a1590b5df2
Robert Nay <rsn.nay@gmail.com>
2019-10-05 18:57:45 -0600

    Switch DashboardList to functional component with apollo hook

--- client/src/components/student/DashboardList.js
+++ client/src/components/student/DashboardList.js
@@ -1,26 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Redirect } from 'react-router';
-import { graphql } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 import gql from 'graphql-tag';
 
-import compose from '../../compose';
 import withAuthCheck from '../shared/AuthCheck';
 
 import PageTitle from '../shared/PageTitle';
 import ErrorBox from '../shared/ErrorBox';
 import LoadingBox from '../shared/LoadingBox';
 
+// Get the student’s current courses
+const STUDENT_QUERY = gql`
+  query studentQuery {
+    currentStudent {
+      id
+      name
+      courses {
+        id
+        title
+      }
+    }
+  }
+`;
+
 /**
  * This page displays cards for all of a students’s courses to navigate to
  * a specific course. If only one course is available, it redirects to that course.
  */
-const DashboardList = ({ studentQuery }) => {
-  if (studentQuery && !studentQuery.currentStudent) {
+const DashboardList = () => {
+  const { error, data } = useQuery(STUDENT_QUERY, {
+    options: { fetchPolicy: 'cache-and-network' },
+  });
+
+  if (!data || !data.currentStudent) {
     return <LoadingBox />;
   }
 
-  if (studentQuery && studentQuery.error) {
+  if (error) {
     return (
       <ErrorBox>
         <p>Couldn’t load dashboard. Please try again later.</p>
@@ -28,23 +45,16 @@ const DashboardList = ({ studentQuery }) => {
     );
   }
 
-  const { courses } = studentQuery.currentStudent;
-  const studentName = studentQuery.currentStudent.name;
+  const { courses, name: studentName } = data.currentStudent;
 
   // If there's only one course, redirect to that dashboard
   if (courses.length === 1) {
-    return (
-      <Redirect
-        to={{
-          pathname: `/student/dashboard/${courses[0].id}`,
-        }}
-      />
-    );
+    return <Redirect to={{ pathname: `/student/dashboard/${courses[0].id}` }} />;
   }
 
   return (
     <section className="section">
-      <PageTitle title='wadayano | My Courses'/>
+      <PageTitle title="wadayano | My Courses" />
       <div className="container">
         <h3 className="title is-3">
           My Courses
@@ -61,14 +71,12 @@ const DashboardList = ({ studentQuery }) => {
               <Link to={`/student/dashboard/${course.id}`} className="tile is-child box">
                 <p className="title">{course.title}</p>
                 <hr />
-                <div className="content">
-                  <span className="button is-primary">
-                    <span className="icon">
-                      <i className="fas fa-rocket" />
-                    </span>
-                    <span>Go to Dashboard</span>
+                <span className="button is-primary">
+                  <span className="icon">
+                    <i className="fas fa-rocket" />
                   </span>
-                </div>
+                  <span>Go to Dashboard</span>
+                </span>
               </Link>
             </div>
           ))}
@@ -78,26 +86,4 @@ const DashboardList = ({ studentQuery }) => {
   );
 };
 
-// Get the student’s current courses
-const STUDENT_QUERY = gql`
-  query studentQuery {
-    currentStudent {
-      id
-      name
-      courses {
-        id
-        title
-      }
-    }
-  }
-`;
-
-export default withAuthCheck(
-  compose(
-    graphql(STUDENT_QUERY, {
-      name: 'studentQuery',
-      options: { fetchPolicy: 'cache-and-network' },
-    })
-  )(DashboardList),
-  { student: true }
-);
+export default withAuthCheck(DashboardList, { student: true });
