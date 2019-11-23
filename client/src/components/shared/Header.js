837576667eafeece06d6ef4240437bc8b8b91d90
Robert Nay <rsn.nay@gmail.com>
2019-11-23 15:25:13 -0700

    Migrate Header

--- client/src/components/shared/Header.js
+++ client/src/components/shared/Header.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { withRouter } from 'react-router';
+import React, { useCallback } from 'react';
+import { Link, NavLink, useHistory } from 'react-router-dom';
 import ButterToast from './Toast';
 import Logo from '../../logo_title.svg';
 
@@ -18,94 +17,93 @@ function toggleMenu(e) {
 /**
  * Main wadayano header to show applicable nav links based on current role.
  */
-class Header extends Component {
-  logOut() {
+const Header = () => {
+  const history = useHistory();
+
+  const logOut = useCallback(() => {
     // Remove auth token and role
     localStorage.removeItem(AUTH_TOKEN);
     localStorage.removeItem(AUTH_ROLE);
     // Redirect to welcome page
-    this.props.history.push('/');
+    history.push('/');
     // Reload page to clear apollo cache
     window.location.reload();
-  }
+  }, [history]);
+  const isLoggedIn = !!localStorage.getItem(AUTH_TOKEN);
+  const isInstructor = localStorage.getItem(AUTH_ROLE) === AUTH_ROLE_INSTRUCTOR;
+  const isStudent = localStorage.getItem(AUTH_ROLE) === AUTH_ROLE_STUDENT;
+  return (
+    <nav className="navbar is-light no-select" aria-label="main navigation">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item" style={{ padding: 0 }}>
+          <img src={Logo} alt="wadayano" style={{ maxHeight: '4rem', height: '4rem' }} />
+        </Link>
+        <button
+          className="navbar-burger button is-light"
+          onClick={e => toggleMenu(e)}
+          data-target="navMenu"
+          aria-label="menu"
+          aria-expanded="false"
+          type="button"
+        >
+          {/* Lines of the hamburger */}
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </button>
+      </div>
 
-  render() {
-    this.isLoggedIn = !!localStorage.getItem(AUTH_TOKEN);
-    this.isInstructor = localStorage.getItem(AUTH_ROLE) === AUTH_ROLE_INSTRUCTOR;
-    this.isStudent = localStorage.getItem(AUTH_ROLE) === AUTH_ROLE_STUDENT;
-    return (
-      <nav className="navbar is-light no-select" aria-label="main navigation">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item" style={{ padding: 0 }}>
-            <img src={Logo} alt="wadayano" style={{ maxHeight: '4rem', height: '4rem' }} />
-          </Link>
-          <button
-            className="navbar-burger button is-light"
-            onClick={e => toggleMenu(e)}
-            data-target="navMenu"
-            aria-label="menu"
-            aria-expanded="false"
-            type="button"
-          >
-            {/* Lines of the hamburger */}
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </button>
+      <div id="header-main-menu" className="navbar-menu">
+        <div className="navbar-start">
+          {isInstructor && (
+            <NavLink activeClassName="is-active" to="/instructor/courses" className="navbar-item">
+              Courses
+            </NavLink>
+          )}
+          {isInstructor && (
+            <NavLink activeClassName="is-active" to="/instructor/profile" className="navbar-item">
+              Profile
+            </NavLink>
+          )}
+          {isStudent && (
+            <NavLink activeClassName="is-active" to="/student/dashboard" className="navbar-item">
+              Dashboard
+            </NavLink>
+          )}
         </div>
 
-        <div id="header-main-menu" className="navbar-menu">
-          <div className="navbar-start">
-            {this.isInstructor && (
-              <NavLink activeClassName="is-active" to="/instructor/courses" className="navbar-item">
-                Courses
-              </NavLink>
+        <div className="navbar-end">
+          {isInstructor && (
+            <NavLink activeClassName="is-active" to="/instructor/help" className="navbar-item">
+              Help
+            </NavLink>
+          )}
+          {isLoggedIn && (
+            <NavLink
+              activeClassName="is-active is-pulled-right"
+              to="/feedback"
+              className="navbar-item"
+            >
+              Send Feedback
+            </NavLink>
+          )}
+          <div className="navbar-item">
+            {isLoggedIn ? (
+              <button className="button is-outlined" onClick={logOut} type="button">
+                Log Out
+              </button>
+            ) : (
+              <Link to="/login" className="button is-info is-outlined">
+                Log In
+              </Link>
             )}
-            {this.isInstructor && (
-              <NavLink activeClassName="is-active" to="/instructor/profile" className="navbar-item">
-                Profile
-              </NavLink>
-            )}
-            {this.isStudent && (
-              <NavLink activeClassName="is-active" to="/student/dashboard" className="navbar-item">
-                Dashboard
-              </NavLink>
-            )}
-          </div>
-
-          <div className="navbar-end">
-            {this.isInstructor && (
-              <NavLink activeClassName="is-active" to="/instructor/help" className="navbar-item">
-                Help
-              </NavLink>
-            )}
-            {this.isLoggedIn && (
-              <NavLink
-                activeClassName="is-active is-pulled-right"
-                to="/feedback"
-                className="navbar-item"
-              >
-                Send Feedback
-              </NavLink>
-            )}
-            <div className="navbar-item">
-              {this.isLoggedIn ? (
-                <button className="button is-outlined" onClick={() => this.logOut()} type="button">
-                  Log Out
-                </button>
-              ) : (
-                <Link to="/login" className="button is-info is-outlined">
-                  Log In
-                </Link>
-              )}
-            </div>
           </div>
         </div>
+      </div>
 
-        <ButterToast timeout={4000} />
-      </nav>
-    );
-  }
-}
+      <ButterToast timeout={4000} />
+    </nav>
+  );
+};
 
-export default withRouter(Header);
+export default Header;
