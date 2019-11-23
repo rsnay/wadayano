472282a72a3911bb738f24fe45b8a0c9ce4f2af7
Robert Nay <rsn.nay@gmail.com>
2019-11-23 16:50:00 -0700

    Continue component migration

--- client/src/components/instructor/SurveyResults.js
+++ client/src/components/instructor/SurveyResults.js
@@ -1,187 +1,15 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { graphql } from 'react-apollo';
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useQuery } from 'react-apollo';
 import gql from 'graphql-tag';
 import ShowMore from 'react-show-more';
 
-import compose from '../../compose';
 import withAuthCheck from '../shared/AuthCheck';
 import CsvGenerator from './CsvGenerator';
 
 import ErrorBox from '../shared/ErrorBox';
 import Spinner from '../shared/Spinner';
 import Breadcrumbs from '../shared/Breadcrumbs';
-
-class SurveyResults extends Component {
-  _downloadCsv() {
-    // Check that data has loaded
-    if (!this.props.courseQuery || this.props.courseQuery.loading || this.props.courseQuery.error) {
-      alert('The survey data has not loaded. Please refresh the page and try again.');
-      return;
-    }
-
-    const { course } = this.props.courseQuery;
-    const { students } = course;
-    const rows = [];
-
-    // Header row with survey questions
-    let headerRow = ['Student Name'];
-    headerRow = headerRow.concat(course.survey.questions.map(q => q.prompt));
-    rows.push(headerRow);
-
-    // Add row for each student (this is largely duplicated from the table display)
-    students.forEach(student => {
-      let studentRow = [];
-      studentRow.push(student.name);
-      // Determine if student took this course’s survey
-      let result = null;
-      try {
-        result = student.surveyResults.filter(r => r.course.id === course.id)[0];
-      } catch (error) {
-        console.error(error);
-      }
-      // Output answer for each question, if survey was taken
-      if (result) {
-        studentRow = studentRow.concat(
-          course.survey.questions.map(q => {
-            if (result.answers[q.index]) {
-              return q.options.filter(o => o.index === result.answers[q.index])[0].text;
-            }
-            return '';
-          })
-        );
-      }
-      rows.push(studentRow);
-    });
-
-    // Download the generated CSV
-    const csvGenerator = new CsvGenerator(rows, 'course_survey_results.csv', ',', true);
-    csvGenerator.download(true);
-  }
-
-  render() {
-    if (this.props.courseQuery && this.props.courseQuery.loading) {
-      return <Spinner />;
-    }
-
-    if (this.props.courseQuery && this.props.courseQuery.error) {
-      return (
-        <ErrorBox>
-          <p>Couldn’t load survey results</p>
-        </ErrorBox>
-      );
-    }
-
-    const { course } = this.props.courseQuery;
-    const { students } = course;
-
-    let resultsTable;
-    if (students.length === 0) {
-      resultsTable = (
-        <p className="notification is-light">
-          There are no students enrolled in this course. When a student launches the survey from
-          their LMS, he/she will be automatically enrolled.
-        </p>
-      );
-    } else {
-      resultsTable = (
-        <div className="table-wrapper">
-          <table className="table is-striped is-fullwidth survey-results-table">
-            <thead>
-              <tr className="sticky-header">
-                <th>Student Name</th>
-                {course.survey.questions.map(q => (
-                  <th key={q.index}>
-                    <ShowMore>{q.prompt}</ShowMore>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {students.map(student => {
-                // Determine if student took this course’s survey
-                let result = null;
-                try {
-                  result = student.surveyResults.filter(r => r.course.id === course.id)[0];
-                } catch (error) {
-                  console.error(error);
-                }
-                // Output answer for each question, if survey was taken
-                return (
-                  <tr key={student.id}>
-                    <td style={{ whiteSpace: 'nowrap' }}>{student.name}</td>
-                    {result ? (
-                      course.survey.questions.map(q => (
-                        <td key={q.index}>
-                          {result.answers[q.index] ? (
-                            q.options.filter(o => o.index === result.answers[q.index])[0].text
-                          ) : (
-                            <i>n/a</i>
-                          )}
-                        </td>
-                      ))
-                    ) : (
-                      <td colSpan={course.survey.questions.length}>
-                        <i>Survey not taken</i>
-                      </td>
-                    )}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      );
-    }
-
-    return (
-      <div>
-        <section className="section">
-          <div className="container">
-            <Breadcrumbs
-              links={[
-                { to: '/instructor/courses', title: 'Courses' },
-                { to: `/instructor/course/${course.id}`, title: course.title },
-                {
-                  to: `/instructor/survey/results/${course.id}`,
-                  title: 'Survey Results',
-                  active: true,
-                },
-              ]}
-            />
-
-            <h3 className="title is-3 is-inline">Survey Results</h3>
-
-            {students.length > 0 && (
-              <button
-                className="button is-light is-pulled-right"
-                onClick={() => this._downloadCsv()}
-                type="button"
-              >
-                <span className="icon">
-                  <i className="fas fa-download" />
-                </span>
-                <span>Download Results as CSV</span>
-              </button>
-            )}
-          </div>
-        </section>
-
-        {resultsTable}
-
-        <hr />
-        <div className="field is-grouped">
-          <p className="control">
-            <Link className="button" to={`/instructor/course/${course.id}`}>
-              Return to Course
-            </Link>
-          </p>
-        </div>
-        <br />
-      </div>
-    );
-  }
-}
 
 // Get the course information
 const COURSE_QUERY = gql`
@@ -205,14 +33,167 @@ const COURSE_QUERY = gql`
   }
 `;
 
-export default withAuthCheck(
-  compose(
-    graphql(COURSE_QUERY, {
-      name: 'courseQuery',
-      options: props => {
-        return { variables: { id: props.match.params.courseId } };
-      },
-    })
-  )(SurveyResults),
-  { instructor: true }
-);
+const SurveyResults = () => {
+  const { courseId } = useParams();
+  const { loading, error, data } = useQuery(COURSE_QUERY, { variables: { id: courseId } });
+
+  const downloadCsv = () => {
+    // Check that data has loaded
+    if (loading || error || !data || !data.course) {
+      alert('The survey data has not loaded. Please refresh the page and try again.');
+    }
+
+    const { course } = data;
+    const { students } = course;
+    const rows = [];
+
+    // Header row with survey questions
+    let headerRow = ['Student Name'];
+    headerRow = headerRow.concat(course.survey.questions.map(q => q.prompt));
+    rows.push(headerRow);
+
+    // Add row for each student (this is largely duplicated from the table display)
+    students.forEach(student => {
+      let studentRow = [];
+      studentRow.push(student.name);
+      // Determine if student took this course’s survey
+      let result = null;
+      try {
+        [result] = student.surveyResults.filter(r => r.course.id === courseId);
+      } catch (err) {
+        console.error(err);
+      }
+      // Output answer for each question, if survey was taken
+      if (result) {
+        studentRow = studentRow.concat(
+          course.survey.questions.map(q => {
+            if (result.answers[q.index]) {
+              return q.options.filter(o => o.index === result.answers[q.index])[0].text;
+            }
+            return '';
+          })
+        );
+      }
+      rows.push(studentRow);
+    });
+
+    // Download the generated CSV
+    const csvGenerator = new CsvGenerator(rows, 'course_survey_results.csv', ',', true);
+    csvGenerator.download(true);
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (error) {
+    return (
+      <ErrorBox>
+        <p>Couldn’t load survey results</p>
+      </ErrorBox>
+    );
+  }
+
+  const { course } = data;
+  const { students } = course;
+
+  let resultsTable;
+  if (students.length === 0) {
+    resultsTable = (
+      <p className="notification is-light">
+        There are no students enrolled in this course. When a student launches the survey from their
+        LMS, he/she will be automatically enrolled.
+      </p>
+    );
+  } else {
+    resultsTable = (
+      <div className="table-wrapper">
+        <table className="table is-striped is-fullwidth survey-results-table">
+          <thead>
+            <tr className="sticky-header">
+              <th>Student Name</th>
+              {course.survey.questions.map(q => (
+                <th key={q.index}>
+                  <ShowMore>{q.prompt}</ShowMore>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {students.map(student => {
+              // Determine if student took this course’s survey
+              let result = null;
+              try {
+                [result] = student.surveyResults.filter(r => r.course.id === courseId);
+              } catch (err) {
+                console.error(err);
+              }
+              // Output answer for each question, if survey was taken
+              return (
+                <tr key={student.id}>
+                  <td style={{ whiteSpace: 'nowrap' }}>{student.name}</td>
+                  {result ? (
+                    course.survey.questions.map(q => (
+                      <td key={q.index}>
+                        {result.answers[q.index] ? (
+                          q.options.filter(o => o.index === result.answers[q.index])[0].text
+                        ) : (
+                          <i>n/a</i>
+                        )}
+                      </td>
+                    ))
+                  ) : (
+                    <td colSpan={course.survey.questions.length}>
+                      <i>Survey not taken</i>
+                    </td>
+                  )}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <section className="section">
+        <div className="container">
+          <Breadcrumbs
+            links={[
+              { to: '/instructor/courses', title: 'Courses' },
+              { to: `/instructor/course/${course.id}`, title: course.title },
+              {
+                to: `/instructor/survey/results/${course.id}`,
+                title: 'Survey Results',
+                active: true,
+              },
+            ]}
+          />
+
+          <h3 className="title is-3 is-inline">Survey Results</h3>
+
+          {students.length > 0 && (
+            <button className="button is-light is-pulled-right" onClick={downloadCsv} type="button">
+              <span className="icon">
+                <i className="fas fa-download" />
+              </span>
+              <span>Download Results as CSV</span>
+            </button>
+          )}
+        </div>
+      </section>
+
+      {resultsTable}
+
+      <hr />
+      <Link className="button" to={`/instructor/course/${course.id}`}>
+        Return to Course
+      </Link>
+      <br />
+    </div>
+  );
+};
+
+export default withAuthCheck(SurveyResults, { instructor: true });
--- client/src/components/shared/Welcome.js
+++ client/src/components/shared/Welcome.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 import { AUTH_ROLE, AUTH_ROLE_STUDENT, AUTH_ROLE_INSTRUCTOR } from '../../constants';
 
@@ -12,131 +12,132 @@ import InsightsImage from '../../img/welcome_insights.png';
  * The page displayed at the root path (e.g. https://wadayano.com/ )
  * Redirects to the student or instructor dashboard, or offers login/signup options and information about wadayano
  */
-export default class Welcome extends Component {
-  componentDidMount() {
-    const authRole = localStorage.getItem(AUTH_ROLE);
-    if (authRole === AUTH_ROLE_STUDENT) {
-      this.props.history.replace('/student/dashboard');
-      return;
-    }
-    if (authRole === AUTH_ROLE_INSTRUCTOR) {
-      this.props.history.replace('/instructor/courses');
-    }
+const Welcome = () => {
+  const history = useHistory();
+
+  const authRole = localStorage.getItem(AUTH_ROLE);
+  if (authRole === AUTH_ROLE_STUDENT) {
+    history.replace('/student/dashboard');
+    return null;
+  }
+  if (authRole === AUTH_ROLE_INSTRUCTOR) {
+    history.replace('/instructor/courses');
+    return null;
   }
 
-  render() {
-    const authButtons = (
-      <p className="buttons is-centered auth-buttons">
-        <Link to="/login" className="button is-info">
-          <span className="icon">
-            <i className="fas fa-sign-in-alt" />
-          </span>
-          <span>Log In</span>
-        </Link>
-        <Link to="/signup" className="button is-info">
-          <span className="icon">
-            <i className="fas fa-user-plus" />
-          </span>
-          <span>Sign Up</span>
-        </Link>
-      </p>
-    );
+  const authButtons = (
+    <p className="buttons is-centered auth-buttons">
+      <Link to="/login" className="button is-info">
+        <span className="icon">
+          <i className="fas fa-sign-in-alt" />
+        </span>
+        <span>Log In</span>
+      </Link>
+      <Link to="/signup" className="button is-info">
+        <span className="icon">
+          <i className="fas fa-user-plus" />
+        </span>
+        <span>Sign Up</span>
+      </Link>
+    </p>
+  );
 
-    return (
-      <main>
-        <section className="hero is-bold is-primary">
-          <div className="hero-body">
-            <div className="container">
-              <h1 className="title">
-                <img
-                  src={Logo}
-                  alt="wadayano"
-                  style={{ maxHeight: '6rem', height: '6rem', marginLeft: '-2rem' }}
-                />
-              </h1>
-              <h2 className="subtitle">Supporting knowledge monitoring development</h2>
-            </div>
+  return (
+    <main>
+      <section className="hero is-bold is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              <img
+                src={Logo}
+                alt="wadayano"
+                style={{ maxHeight: '6rem', height: '6rem', marginLeft: '-2rem' }}
+              />
+            </h1>
+            <h2 className="subtitle">Supporting knowledge monitoring development</h2>
           </div>
-        </section>
-
-        <div className="container welcome-page">
-          {authButtons}
-
-          <div className="section tile is-ancestor">
-            <div className="tile is-parent">
-              <article className="tile is-child box">
-                <h2 className="title">Student Quizzes</h2>
-                <p className="content">
-                  Help your students better monitor <b>what they know</b> and <b>what they don’t</b>{' '}
-                  know in your courses with online quizzes.
-                  <br />
-                  <br />
-                  We integrate with popular learning management systems via LTI, creating a seamless
-                  experience for your students.
-                  <br />
-                  <br />
-                  <img src={QuizzesImage} alt="Preview of a mobile-friendly wadayano quiz" />
-                </p>
-              </article>
-            </div>
-
-            <div className="tile is-parent">
-              <article className="tile is-child box">
-                <h2 className="title">Confidence Analysis</h2>
-                <p className="content">
-                  Students estimate how well they know each quiz concept and indicate whether they
-                  are confident in their response to each question.
-                  <br />
-                  <br />
-                  In addition to getting a normal quiz score, they receive a wadayano score that
-                  indicates how accurate their predictions were.
-                  <br />
-                  <br />
-                  <img
-                    src={ConfidenceImage}
-                    alt="Confidence analysis and wadayano scores to help students improve their knowledge monitoring abilities"
-                  />
-                </p>
-              </article>
-            </div>
-
-            <div className="tile is-parent">
-              <article className="tile is-child box">
-                <h2 className="title">Instructor Insights</h2>
-                <p className="content">
-                  Review predicted and wadayano scores by quiz and concept to see where students
-                  over- and underestimate their own knowledge.
-                  <br />
-                  <br />
-                  Make informed course adjustments and ensure that the balance of quiz items meets
-                  students’ needs.
-                  <br />
-                  <br />
-                  <img
-                    src={InsightsImage}
-                    alt="Detailed instructor insights provide predicted, actual, and wadayano scores for quizzes and concepts."
-                  />
-                </p>
-              </article>
-            </div>
-          </div>
-
-          <section className="section" style={{ marginTop: '-2rem', paddingTop: 0 }}>
-            <h2 className="title is-3">Why Knowledge Monitoring?</h2>
-            Knowledge monitoring (KM) is a predictor of broader success like GPA and high school
-            dropout rate. It’s also foundational to other metacognitive skills—having an accurate
-            sense of what you do and don’t know enables you to develop plans and strategies that
-            make effective use of limited study time and resources.&nbsp;
-            <Link to="/signup" className="has-text-info is-text">
-              Get started with wadayano for free
-              <span className="icon" style={{ verticalAlign: 'top' }}>
-                <i className="fas fa-chevron-right" />
-              </span>
-            </Link>
-            {authButtons}
-          </section>
         </div>
-      </main>
-    );
-  }
-}
+      </section>
+
+      <div className="container welcome-page">
+        {authButtons}
+
+        <div className="section tile is-ancestor">
+          <div className="tile is-parent">
+            <article className="tile is-child box">
+              <h2 className="title">Student Quizzes</h2>
+              <p className="content">
+                Help your students better monitor <b>what they know</b> and <b>what they don’t</b>{' '}
+                know in your courses with online quizzes.
+                <br />
+                <br />
+                We integrate with popular learning management systems via LTI, creating a seamless
+                experience for your students.
+                <br />
+                <br />
+                <img src={QuizzesImage} alt="Preview of a mobile-friendly wadayano quiz" />
+              </p>
+            </article>
+          </div>
+
+          <div className="tile is-parent">
+            <article className="tile is-child box">
+              <h2 className="title">Confidence Analysis</h2>
+              <p className="content">
+                Students estimate how well they know each quiz concept and indicate whether they are
+                confident in their response to each question.
+                <br />
+                <br />
+                In addition to getting a normal quiz score, they receive a wadayano score that
+                indicates how accurate their predictions were.
+                <br />
+                <br />
+                <img
+                  src={ConfidenceImage}
+                  alt="Confidence analysis and wadayano scores to help students improve their knowledge monitoring abilities"
+                />
+              </p>
+            </article>
+          </div>
+
+          <div className="tile is-parent">
+            <article className="tile is-child box">
+              <h2 className="title">Instructor Insights</h2>
+              <p className="content">
+                Review predicted and wadayano scores by quiz and concept to see where students over-
+                and underestimate their own knowledge.
+                <br />
+                <br />
+                Make informed course adjustments and ensure that the balance of quiz items meets
+                students’ needs.
+                <br />
+                <br />
+                <img
+                  src={InsightsImage}
+                  alt="Detailed instructor insights provide predicted, actual, and wadayano scores for quizzes and concepts."
+                />
+              </p>
+            </article>
+          </div>
+        </div>
+
+        <section className="section" style={{ marginTop: '-2rem', paddingTop: 0 }}>
+          <h2 className="title is-3">Why Knowledge Monitoring?</h2>
+          Knowledge monitoring (KM) is a predictor of broader success like GPA and high school
+          dropout rate. It’s also foundational to other metacognitive skills—having an accurate
+          sense of what you do and don’t know enables you to develop plans and strategies that make
+          effective use of limited study time and resources.&nbsp;
+          <Link to="/signup" className="has-text-info is-text">
+            Get started with wadayano for free
+            <span className="icon" style={{ verticalAlign: 'top' }}>
+              <i className="fas fa-chevron-right" />
+            </span>
+          </Link>
+          {authButtons}
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default Welcome;
