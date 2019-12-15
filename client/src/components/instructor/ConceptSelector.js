21da5f4862c41278092de7f7e13ae0a003f32002
Robert Nay <rsn.nay@gmail.com>
2019-12-14 23:38:16 -0700

    Migrate ConceptSelector to hooks

--- client/src/components/instructor/ConceptSelector.js
+++ client/src/components/instructor/ConceptSelector.js
@@ -1,109 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'react-apollo';
+import { useQuery } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const SUGGESTION_COUNT = 10;
-
-class ConceptSelector extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showSuggestions: false,
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-  }
-
-  setConcept(concept, showSuggestions = false) {
-    this.setState({ showSuggestions });
-    if (this.props.onChange) {
-      this.props.onChange(concept);
-    }
-  }
-
-  handleFocus() {
-    this.setState({ showSuggestions: true });
-  }
-
-  handleInputChange(event) {
-    this.setConcept(event.target.value, true);
-  }
-
-  render() {
-    // Prepare filtering (non-lowercase is used in display)
-    const filterText = this.props.concept.trim();
-    const lowerCaseFilterText = filterText.toLowerCase();
-    // Use course concepts, if loaded
-    const { conceptsQuery } = this.props;
-    const concepts =
-      conceptsQuery && !conceptsQuery.error && conceptsQuery.courseConcepts
-        ? conceptsQuery.courseConcepts
-        : [];
-    // Perform filtering
-    const filteredConcepts = concepts.filter(concept =>
-      concept.toLowerCase().includes(lowerCaseFilterText)
-    );
-
-    const suggestionsList = this.state.showSuggestions ? (
-      <span className="concept-suggestions-list">
-        {filteredConcepts.slice(0, SUGGESTION_COUNT).map(concept => (
-          <button
-            key={concept}
-            className="concept-tag tag is-light"
-            onClick={() => this.setConcept(concept, false)}
-            type="button"
-          >
-            {concept}
-          </button>
-        ))}
-        {/* Add current filter text, if not empty or already present */}
-        {filterText !== '' && filteredConcepts.indexOf(filterText) === -1 && (
-          <button
-            className="concept-tag tag is-link"
-            onClick={() => this.setConcept(filterText, false)}
-            type="button"
-          >
-            + {filterText}
-          </button>
-        )}
-      </span>
-    ) : (
-      <span className="concept-suggestions-list" />
-    );
-
-    return (
-      <>
-        <input
-          value={this.props.concept}
-          autoFocus={this.props.autoFocus}
-          onFocus={this.handleFocus}
-          onChange={this.handleInputChange}
-          className="input is-inline"
-          type="text"
-          maxLength={200}
-          placeholder={this.props.placeholder}
-        />
-        {suggestionsList}
-      </>
-    );
-  }
-}
-
-ConceptSelector.propTypes = {
-  concept: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  autoFocus: PropTypes.bool,
-  // Concept suggestions will be fetched from all quizzes in the given course
-  courseId: PropTypes.string,
-  placeholder: PropTypes.string,
-};
-
-ConceptSelector.defaultProps = {
-  placeholder: 'Concept',
-  autoFocus: false,
-};
 
 // Get the concepts from the whole course
 const CONCEPTS_QUERY = gql`
@@ -112,9 +12,85 @@ const CONCEPTS_QUERY = gql`
   }
 `;
 
-export default graphql(CONCEPTS_QUERY, {
-  name: 'conceptsQuery',
-  options: props => {
-    return { fetchPolicy: 'cache-and-network', variables: { id: props.courseId } };
-  },
-})(ConceptSelector);
+/**
+ * Component used in QuestionEditor to suggest concepts from the current
+ * course (typeahead) or create a new concept for the question being edited.
+ * (Concepts aren’t stored outside of questions, so “creating” a new concept
+ * doesn’t do anything in the database.)
+ */
+const ConceptSelector = ({ courseId, concept, onChange, autoFocus = false }) => {
+  const { data } = useQuery(CONCEPTS_QUERY, {
+    fetchPolicy: 'cache-and-network',
+    variables: { id: courseId },
+  });
+
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const setConcept = (newConcept, newShowSuggestions = false) => {
+    setShowSuggestions(newShowSuggestions);
+    if (onChange) {
+      onChange(newConcept);
+    }
+  };
+
+  // Prepare filtering (non-lowercase is used in display)
+  const filterText = concept.trim();
+  const lowerCaseFilterText = filterText.toLowerCase();
+  // Use course concepts, if loaded
+  const concepts = data && data.courseConcepts ? data.courseConcepts : [];
+  // Perform filtering
+  const filteredConcepts = concepts.filter(c => c.toLowerCase().includes(lowerCaseFilterText));
+
+  const suggestionsList = showSuggestions ? (
+    <span className="concept-suggestions-list">
+      {filteredConcepts.slice(0, SUGGESTION_COUNT).map(c => (
+        <button
+          key={c}
+          className="concept-tag tag is-light"
+          onClick={() => setConcept(c)}
+          type="button"
+        >
+          {c}
+        </button>
+      ))}
+      {/* Add current filter text, if not empty or already present */}
+      {filterText !== '' && filteredConcepts.indexOf(filterText) === -1 && (
+        <button
+          className="concept-tag tag is-link"
+          onClick={() => setConcept(filterText)}
+          type="button"
+        >
+          + {filterText}
+        </button>
+      )}
+    </span>
+  ) : (
+    <span className="concept-suggestions-list" />
+  );
+
+  return (
+    <>
+      <input
+        value={concept}
+        autoFocus={autoFocus}
+        onFocus={() => setShowSuggestions(true)}
+        onChange={e => setConcept(e.target.value, true)}
+        className="input is-inline"
+        type="text"
+        maxLength={200}
+        placeholder="Concept"
+      />
+      {suggestionsList}
+    </>
+  );
+};
+
+ConceptSelector.propTypes = {
+  concept: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  autoFocus: PropTypes.bool,
+  // Concept suggestions will be fetched from all quizzes in the given course
+  courseId: PropTypes.string,
+};
+
+export default ConceptSelector;
