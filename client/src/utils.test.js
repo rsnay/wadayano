9eab1c311d0aab11b93b410641339fd2a29c4b37
Robert Nay <rsn.nay@gmail.com>
2019-12-21 22:12:17 -0700

    Add tests for filterByConcept

--- client/src/utils.test.js
+++ client/src/utils.test.js
@@ -37,3 +37,50 @@ describe('stripTags', () => {
     expect(stripTags(html)).toBe(expected);
   });
 });
+
+describe('filterByConcept', () => {
+  const quizAttempt = {
+    questionAttempts: [
+      {
+        id: 'qa1',
+        isCorrect: true,
+        isConfident: false,
+        question: { id: 'q1', concept: 'c1' },
+      },
+      {
+        id: 'qa2',
+        isCorrect: false,
+        isConfident: false,
+        question: { id: 'q2', concept: 'c1' },
+      },
+      {
+        id: 'qa3',
+        isCorrect: true,
+        isConfident: true,
+        question: { id: 'q3', concept: 'c2' },
+      },
+    ],
+  };
+  test('filters properly by concept that is in quiz', () => {
+    const result1 = filterByConcept(quizAttempt, 'c1');
+    expect(result1).toContain(quizAttempt.questionAttempts[0]);
+    expect(result1).toContain(quizAttempt.questionAttempts[1]);
+    expect(result1).not.toContain(quizAttempt.questionAttempts[2]);
+
+    const result2 = filterByConcept(quizAttempt, 'c2');
+    expect(result2).not.toContain(quizAttempt.questionAttempts[0]);
+    expect(result2).not.toContain(quizAttempt.questionAttempts[1]);
+    expect(result2).toContain(quizAttempt.questionAttempts[2]);
+  });
+  test('returns empty array for concept not in quiz', () => {
+    const result = filterByConcept(quizAttempt, 'other concept');
+    expect(result).toHaveLength(0);
+  });
+  test('returns all question attempts if concept is null', () => {
+    const result = filterByConcept(quizAttempt, null);
+    expect(result).toHaveLength(3);
+    expect(result).toContain(quizAttempt.questionAttempts[0]);
+    expect(result).toContain(quizAttempt.questionAttempts[1]);
+    expect(result).toContain(quizAttempt.questionAttempts[2]);
+  });
+});
